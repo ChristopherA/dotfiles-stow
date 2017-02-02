ff7969ddf7ff854327c03f9e1c281d7cf0a58957
FIRSTNAME LASTNAME <USERNAME@COMPANY.COM>
2017-02-01 18:19:31 -0800

    Some bitcoin aliases & changes to gpg .gitignore

--- 5-macos/.profile.d/9-macos-bash-aliases.sh
+++ 5-macos/.profile.d/9-macos-bash-aliases.sh
@@ -15,3 +15,12 @@ alias flushdns="dscacheutil -flushcache"
 # Empty the Trash on all mounted volumes and the main HDD
 # Also, clear Apple’s System Logs to improve shell startup speed
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
+
+# Bitcoin related
+
+#alias btcdir="cd ~/.bitcoin/" #linux default bitcoind path
+alias btcdir="cd ~/Library/Application\ Support/Bitcoin/" #mac default bitcoind path
+alias bc="bitcoin-cli"
+alias bd="bitcoind"
+alias btcinfo='bitcoin-cli getinfo | egrep "\"version\"|balance|blocks|connections|errors"'
+alias btcblock="echo `bitcoin-cli getblockcount 2>&1`/`wget -O - http://blockchain.info/q/getblockcount 2>/dev/null`"
